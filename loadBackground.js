9eaaddf2adb7a001846961736ea43de7d651e6bb
Joe Fang <joefang20031128@hotmail.com>
2020-06-27 19:59:34 +0800

    update

--- loadBackground.js
+++ loadBackground.js
@@ -18,56 +18,36 @@
  */
 
 (() => {
-    const rootUrl = 'https://cdn.jsdelivr.net/';
-    const backgroundImageList = [
-        {
-            name: 'Galaxy',
-            fallback: 'gh/urlib/bin_0@f5a296f9/fe/94/83/5f/fe94835f3f8985e5965b9fe974d1917858affba03182c2aef5071ffbfcaee61c.png',
-            webp: 'gh/urlib/bin_0@f5a296f9/95/11/c2/d9/9511c2d95bdce8999a519c9f7c1910b316c4c6728bf6b7ef7ee9cdfd1a0faeb1.webp'
-        },
-        {
-            name: 'Purple Galaxy',
-            fallback: 'gh/urlib/bin_0@f5a296f9/db/2e/1e/54/db2e1e54c1152b4dfef159d9668427876d7f5cd0a1a8e8caa83afa83cb4914de.png',
-            webp: 'gh/urlib/bin_0@f5a296f9/fa/8b/c2/54/fa8bc254ad36808b3bf8d110e11678acb4206c07f12f95ff5d23d8920268259d.webp'
-        },
-        {
-            name: 'milky-way-2695569',
-            fallback: 'gh/urlib/bin_0@9df0c1b8/4e/4a/cc/27/4e4acc27fe5141b1569159fd67fd0a3f75bd49d473db7cb037cc34b26ab9345b.jpg',
-            webp: 'gh/urlib/bin_0@9df0c1b8/04/63/7c/ad/04637cadfd2f562927ba40237a4dddf8e4e777a40332fab19f1ab6ef84408fa1.webp'
-        },
-        {
-            name: 'ocean-3605547',
-            fallback: 'gh/urlib/bin_0@9df0c1b8/6a/81/d7/86/6a81d7861a3edeb015f3dd815a1af17f739ac25e6e0cddecb98f818c6de1e030.jpg',
-            webp: 'gh/urlib/bin_0@9df0c1b8/09/61/ed/d9/0961edd9edc1a2b0c98fdc6e9254219e1bd4af77eeef8a1f5ee9baa89503730b.webp'
-        },
-        {
-            name: 'Appearance',
-            fallback: 'gh/urlib/bin_0@3249ede2/12/f5/17/e5/12f517e5efa053304b4c4ca213f2415ff98a92f0ce29e8479df9ca8669445c01.jpg',
-            webp: 'gh/urlib/bin_0@3249ede2/12/75/b3/7e/1275b37e49e600dfe38fb999c55b043e4179f4e2159f8c03dfcdec831eeb639c.webp'
-        },
-        {
-            name: 'paul-morris-167780',
-            fallback: 'gh/urlib/bin_0@3249ede2/cd/44/fd/ff/cd44fdff734c4e72c692829cb04173bba0449bf23ad691e4b2639a21fdf8975a.jpg',
-            webp: 'gh/urlib/bin_0@3249ede2/30/9e/38/01/309e380135e0e0ef87bf794003886f85b3931d744cde897d32226a647b724054.webp'
-        }
-    ];
+    const imageListJson = 'json/loadBackground.imageList.d00f8cce.json';
 
-    const completeUrl = (partialUrl) => {
-        return `${rootUrl}${partialUrl}`;
-    };
-    const checkWebpSupport = () => {
-        return document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0;
-    };
-    const isWebpSupported = checkWebpSupport();
-    const randomChoice = (list) => {
+    const fetchImageList = () => {
+        let imageList = [{ fallback: 'blank.gif' }];
+        const reportError = err => {
+            console.warn(`fetchImageList: An error occured: ${err.message}. Use blank.gif as fallback. `);
+        };
+        fetch(imageListJson, {
+            cache: 'no-store'
+        })
+            .then(res => {
+                if (res.ok) {
+                    imageList = res.json();
+                } else {
+                    reportError('Network Error');
+                }
+            })
+            .catch(err => reportError(err));
+        return imageList;
+    }
+    const randomChoice = list => {
         list.push(list.shift());
         return list[Math.floor(Math.random() * list.length)];
     };
-    const imageChoice = (isWebpSupported) => {
+    const backgroundImageList = window.backgroundImageList || fetchImageList();
+    const isWebpSupported = (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0);
+    const imageUrl = (() => {
         const image = randomChoice(backgroundImageList);
-        return completeUrl(isWebpSupported ? image.webp : image.fallback);
-    };
-    const imageUrl = imageChoice(isWebpSupported);
+        return isWebpSupported && image.webp ? image.webp : image.fallback;
+    })();
 
     window.addEventListener('load', () => {
         const style = document.body.style;
