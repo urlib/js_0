8d2e57c528fdee8337e923df8c3a402802df3c8c
Joe Fang <joefang20031128@hotmail.com>
2020-06-27 22:26:13 +0800

    fix: async

--- loadBackground.js
+++ loadBackground.js
@@ -18,40 +18,45 @@
  */
 
 (() => {
+    // Configurations
     const rootUrl = 'https://cdn.jsdelivr.net/gh/urlib/js_0@latest/';
     const imageListJson = `${rootUrl}json/loadBackground.imageList.d00f8cce.json`;
     const blankGif = 'data:image/gif;base64,R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 
-    const fetchImageList = () => {
-        let imageList = [{ fallback: blankGif }];
+    window.isWebpSupported = window.isWebpSupported || (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0);
+    const fetchImageList = async () => {
         const reportError = err => {
             console.warn(`fetchImageList: An error occured: ${err.message}. Use blank.gif as fallback. `);
         };
-        fetch(imageListJson, {
+        const fallbackImage = [{ fallback: blankGif }];
+        return await fetch(imageListJson, {
             cache: 'no-store'
         })
             .then(res => {
                 if (res.ok) {
-                    imageList = res.json();
+                    return res.json();
                 } else {
                     reportError('Network Error');
+                    return fallbackImage;
                 }
             })
-            .catch(err => reportError(err));
-        return imageList;
-    }
+            .catch(err => {
+                reportError(err);
+                return fallbackImage;
+            });
+    };
     const randomChoice = list => {
         list.push(list.shift());
         return list[Math.floor(Math.random() * list.length)];
     };
-    const backgroundImageList = window.backgroundImageList || fetchImageList();
-    const isWebpSupported = (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0);
-    const imageUrl = (() => {
-        const image = randomChoice(backgroundImageList);
+    const getImageUrl = async () => {
+        window.backgroundImageList = window.backgroundImageList || await fetchImageList();
+        const image = randomChoice(window.backgroundImageList);
         return (isWebpSupported && image.webp) ? image.webp : image.fallback;
-    })();
+    };
 
-    window.addEventListener('load', () => {
+    window.addEventListener('load', async () => {
+        const imageUrl = await getImageUrl();
         const style = document.body.style;
         style.backgroundImage = `url(${imageUrl})`;
         style.backgroundAttachment = 'fixed';
